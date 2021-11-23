f5fd1ebde5afa262091646282e232d105d3812f0
Tobia Rossi <github@tobiarossi.ch>
2021-11-23 08:50:31 +0100

    Simple test code for HW check

--- src/Main.cpp
+++ src/Main.cpp
@@ -19,9 +19,12 @@
 #include "DemoBoard.hpp"
 
 void setup() {
-  // put your setup code here, to run once:
+	pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+	digitalWrite(LED_BUILTIN, LOW);
+	delay(10);
+	digitalWrite(LED_BUILTIN, HIGH);
+	delay(10);
 }
